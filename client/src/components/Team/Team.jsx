39be5027c11af55a56875a270628be1ab7ee97e2
Srijan Das <113245185+thisIsSrijan@users.noreply.github.com>
2025-01-15 16:34:02 +0530

    Update Team.jsx

--- client/src/components/Team/Team.jsx
+++ client/src/components/Team/Team.jsx
@@ -17,28 +17,28 @@ const Team = () => {
   return (
     <div className="bg-black h-full w-full text-white flex flex-col justify-center">
       <section
-  className="flex flex-col items-center justify-start pt-14 w-full h-screen relative"
-  style={{
-    backgroundImage: "url('/public/assets/plinth.png')",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  }}
->
-  <h2
-    className="text-[#A7F818] text-center text-4xl md:text-[70px] relative z-10 lg:text-[90px] xl:text-[120px]"
-    style={{ fontFamily: "Cynatar, sans-serif", letterSpacing: "0.35em" }}
-  >
-    OUR TEAM
-  </h2>
-  <div className="absolute top-0 left-0 w-full h-[30%] bg-gradient-to-t from-transparent to-black"></div>
-  <div className="absolute bottom-0 left-0 w-full h-[30%] bg-gradient-to-t from-black to-transparent"></div>
-  <div className="absolute inset-0 w-full h-full bg-gradient-to-l from-[rgb(0,0,0,0.8)] via-transparent to-[rgba(0,0,0,0.8)]"></div>
-</section>
+        className="flex flex-col items-center justify-center pt-14 w-full h-screen relative"
+        style={{
+          backgroundImage: "url('/public/assets/plinth.png')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <h2
+          className="text-[#A7F818] text-center text-4xl sm:text-[60px] md:text-[85px] relative z-10 lg:text-[100px] xl:text-[140px]"
+          style={{ fontFamily: "Cynatar, sans-serif", letterSpacing: "0.35em" }}
+        >
+          OUR TEAM
+        </h2>
+        <div className="absolute top-0 left-0 w-full h-[30%] bg-gradient-to-t from-transparent to-black"></div>
+        <div className="absolute bottom-0 left-0 w-full h-[30%] bg-gradient-to-t from-black to-transparent"></div>
+        <div className="absolute inset-0 w-full h-full bg-gradient-to-l from-[rgb(0,0,0,0.8)] via-transparent to-[rgba(0,0,0,0.8)]"></div>
+      </section>
 
 
       {/* Centered Image */}
-  
-       
+
+
 
       {/* Various team divisions */}
       <section className="flex flex-col w-full gap-10 font-melete overflow-hidden">
