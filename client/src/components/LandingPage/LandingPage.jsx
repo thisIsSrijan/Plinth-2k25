c2ad2548300da55ae473cd5b0b866599c4376e93
Srijan Das <dassrijan16@gmail.com>
2025-01-16 19:18:48 +0530

    landing page animations

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -2,10 +2,26 @@ import Navbar from "./Navbar";
 import { GlowEllipse } from "../../assets/competition/svg";
 import { FaLocationDot } from "react-icons/fa6";
 import { IoLocationOutline } from "react-icons/io5";
+import { delay, motion } from "framer-motion";
 import './Plinth.css';
 
 
 const LandingPage = () => {
+    const letterContainer = {
+      hidden: { opacity: 1 },
+      visible: {
+        opacity: 1,
+        transition: {
+          staggerChildren: 0.1,
+        },
+      },
+    };
+  
+    const letter = {
+      hidden: { y: 50, opacity: 0 },
+      visible: { y: 0, opacity: 1, transition: { type: "spring", stiffness: 500 } },
+    };
+
     return (
       <div className="w-full min-h-[100vh] uxsm:h-full md:h-full bg-black overflow-hidden flex flex-col justify-center items-center ">
         {/* <Navbar></Navbar> */}
@@ -22,29 +38,50 @@ const LandingPage = () => {
           Your browser does not support the video tag.
         </video>
         <div className="flex flex-col justify-center text-center items-center uxsm:mt-40 md:mt-0  w-full">
-          <div className="flex flex-col justify-center items-center relative mt-[8rem] ">
+          <div className="flex flex-col justify-center items-center relative mt-[7rem] sm:mt-[8rem] ">
             <div
-              className="absolute font-cynatar uxsm:text-s tracking-wide  w-[90rem] c1:text-[20rem] 2xl:text-[15rem] xl:text-[13rem] lg:text-[12rem] md:text-[9rem] sm:text-[8rem] xsm:text-[5rem] uxsm:text-[4.2rem] font-[400] textfamily  plinth hero glitch layers"
+              className="absolute font-cynatar uxsm:text-s tracking-wide  w-[90rem] c1:text-[20rem] 
+              2xl:text-[15rem] xl:text-[13rem] lg:text-[12rem] md:text-[9rem] sm:text-[8rem] xsm:text-[5rem] 
+              uxsm:text-[3.5rem] font-[400] textfamily  plinth hero glitch layers"
               data-text="PLINTH 25"
             >
               PLINTH 25
             </div>
             <div
-              className="w-[90rem] font-cynatar mr-4 c1:text-[20rem] tracking-wide 2xl:text-[15rem] xl:text-[13rem] lg:text-[12rem] md:text-[9rem] sm:text-[8rem] xsm:text-[5rem] uxsm:text-[4.2rem] font-[400] textfamily3 z-1   plinth hero glitch layers "
+              className="w-[90rem] font-cynatar mr-4 c1:text-[20rem] tracking-wide 2xl:text-[15rem] xl:text-[13rem] 
+              lg:text-[12rem] md:text-[9rem] sm:text-[8rem] xsm:text-[5rem] uxsm:text-[3.5rem] font-[400] textfamily3 z-1   
+              plinth hero glitch layers "
               data-text="PLINTH 25"
             >
               PLINTH 25
             </div>
           </div>
-          <div className="c1:text-[4rem] 2xl:text-[2.5rem] xl:text-[2.3rem] lg:text-[2rem] md:text-[1.8rem] sm:text-[1.7rem] xsm:text-[1.3rem] uxsm:text-[1.2rem]  greencolor md:mt-[6rem] uxsm:mt-[8rem] font-melete relative ">
-            24-26 JAN 2025
-          </div>
-          <div className="flex c1:text-[3.5rem] 2xl:text-[2rem] xl:text-[1.8rem] lg:text-[1.5rem] md:text-[1.3rem] sm:text-[1.4rem] xsm:text-[0.8rem] uxsm:text-[0.7rem] text-white greycolor font-melete relative">
-            <span className="greycolor">
-              <IoLocationOutline className=" c1:text-[4rem] xl:text-[2.5rem] lg:text-[2rem] md:text-[2rem] sm:text-[1.5rem] xsm:text-[1.5rem] uxsm:text-[1rem]"></IoLocationOutline>
-            </span>
-            LNMIIT,JAIPUR
-          </div>
+        <motion.div
+          className="c1:text-[4rem] 2xl:text-[2.5rem] xl:text-[2.3rem] lg:text-[2rem] md:text-[1.8rem] xsm:text-[1.2rem] text-[0.8rem] greencolor md:mt-[6rem] uxsm:mt-[8rem] font-melete relative"
+          variants={letterContainer}
+          initial="hidden"
+          animate="visible"
+        >
+          {"24-26 JANUARY 2025".split("").map((char, index) => (
+            <motion.span key={index} variants={letter}>
+              {char === " " ? "\u00A0" : char}
+            </motion.span>
+          ))}
+        </motion.div>
+
+        <motion.div
+          className="flex c1:text-[3.5rem] 2xl:text-[2rem] xl:text-[1.8rem] lg:text-[1.5rem] md:text-[1.3rem] sm:text-[1.4rem] xsm:text-[0.8rem] uxsm:text-[0.7rem] text-white greycolor font-melete relative"
+          variants={letterContainer}
+          initial="hidden"
+          animate="visible"
+          transition={{ delay: 5 }}
+        >
+          {"LNMIIT, JAIPUR".split("").map((char, index) => (
+            <motion.span key={index} variants={letter}>
+              {char === " " ? "\u00A0" : char}
+            </motion.span>
+          ))}
+        </motion.div>
 
           {/* <div className="mt-10 z-10">
                     <button className="w-[15rem] rounded-[2rem] border-2 border-[#666666] bg-[#1A1A1A] text-white py-2 shadow-inner shadow-[#c6c6c6] poppins-medium greycolor">
@@ -65,11 +102,16 @@ const LandingPage = () => {
             <span className="absolute inset-0 border border-white opacity-20 transform scale-x-0 origin-left transition-transform duration-300 ease-in-out hover:scale-x-100"></span>
           </button>
 
-          <div className=" uxsm:w-full absolute uxsm:right-[3.5rem] xsm:right-[5rem] md:-left-[30rem] lg:right-[10rem] xl:right-[13rem] 2xl:right-[15rem] uxl:-left-[40rem] c1:-left-[50rem] font-melete lg:mt-[8rem] md:mt-[6rem] sm:mt-[1rem] xsm:mt-[0rem] uxsm:mt-[0rem]   mb-[5rem] c1:text-[2.3rem] 2xl:text-[1.8rem] xl:text-[1.5rem] lg:text-[1.3rem] md:text-[1.1rem] sm:text-[0.9rem] uxsm:text-[0.6rem] ml-[20rem] flex justify-end items-end greycolor">
-            A CYBER PARADOX
-          </div>
+          <motion.div
+          className="uxsm:w-full absolute uxsm:right-[3.5rem] xsm:right-[5rem] md:-left-[30rem] lg:right-[10rem] xl:right-[13rem] 2xl:right-[15rem] uxl:-left-[40rem] c1:-left-[50rem] font-melete lg:mt-[8rem] md:mt-[6rem] sm:mt-[1rem] xsm:mt-[0rem] uxsm:mt-[0rem] mb-[5rem] c1:text-[2.3rem] 2xl:text-[1.8rem] xl:text-[1.5rem] lg:text-[1.3rem] md:text-[1.1rem] sm:text-[0.9rem] uxsm:text-[0.6rem] ml-[20rem] flex justify-end items-end greycolor"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.5, ease: "easeInOut" }}
+        >
+          A CYBER PARADOX
+        </motion.div>
         </div>
-        <div class="glowing-div"></div>
+        {/* <div class="glowing-div"></div> */}
       </div>
     );
 };
