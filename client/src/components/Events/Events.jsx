2b5979e424c5206bf0711e611cad3578d9527e00
Srijan Das <dassrijan16@gmail.com>
2025-01-17 16:36:24 +0530

    add animations on events

--- client/src/components/Events/Events.jsx
+++ client/src/components/Events/Events.jsx
@@ -11,8 +11,19 @@ import mobCh from "../../assets/Events/mobile-chirag.png";
 import mobsh from "../../assets/Events/mobile-shailey.png";
 import mobar from "../../assets/Events/mobile-arpit.png";
 import imgbox1 from "../../assets/Events/mobileViewBox.jpeg";
+import { motion } from "framer-motion";
 
 const Events = () => {
+  const animationVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+  };
+
+  const animationVariants_bottom = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: -7, transition: { duration: 0.5 } },
+  };
+
   return (
     <div className="w-full h-full overflow-hidden">
       {/* desktop */}
@@ -33,17 +44,44 @@ const Events = () => {
                 }
               }
             >
-              <div className=" absolute  font-cynatar uxsm:text-s tracking-wide c1:text-[20rem] 2xl:text-[14rem] xl:text-[12rem] lg:text-[12rem] md:text-[9rem] sm:text-[8rem] xsm:text-[5rem] uxsm:text-[4.5rem] font-[400] textfamily plinth z-10">
-                DAY 1
+              {/* <div className=" absolute  font-cynatar uxsm:text-s tracking-wide c1:text-[20rem] 2xl:text-[14rem] xl:text-[12rem] lg:text-[12rem] md:text-[9rem] sm:text-[8rem] xsm:text-[5rem] uxsm:text-[4.5rem] font-[400] textfamily plinth z-10">
+                Day 1
               </div>
               <div className="font-cynatar  mr-4 c1:text-[20rem] tracking-wide 2xl:text-[14rem] xl:text-[12rem] lg:text-[12rem] md:text-[9rem] sm:text-[8rem] xsm:text-[5rem] uxsm:text-[4.5rem] font-[400] textfamily3 z-1 plinth z-50">
-                DAY 1
+                Day 1
               </div>
               <div className=" -mt-10 ml-7">
                 <p className="c1:text-[2.3rem] xl:text-[1.5rem] lg:text-[1.2rem] md:text-[1rem] sm:text-[1.2rem] xsm:text-[1rem] text-white font-melete font-[400]">
                   JAN 24,2025
                 </p>
-              </div>
+              </div> */}
+              <motion.div
+                className="absolute font-cynatar uxsm:text-s tracking-wide c1:text-[20rem] 2xl:text-[14rem] xl:text-[12rem] lg:text-[12rem] md:text-[9rem] sm:text-[8rem] xsm:text-[5rem] uxsm:text-[4.5rem] font-[400] textfamily plinth z-10"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                variants={animationVariants_bottom}
+              >
+                Day 1
+              </motion.div>
+              <motion.div
+                className="font-cynatar mr-4 c1:text-[20rem] tracking-wide 2xl:text-[14rem] xl:text-[12rem] lg:text-[12rem] md:text-[9rem] sm:text-[8rem] xsm:text-[5rem] uxsm:text-[4.5rem] font-[400] textfamily3 plinth z-50"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                variants={animationVariants}
+              >
+                Day 1
+              </motion.div>
+              <motion.p
+                className="c1:text-[2.3rem] xl:text-[1.5rem] lg:text-[1.2rem] md:text-[1rem] sm:text-[1.2rem] xsm:text-[1rem] text-white font-melete font-[400] -mt-10 ml-7"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                variants={animationVariants}
+              >
+                JAN 24, 2025
+              </motion.p>
             </div>
 
             <div
@@ -57,32 +95,42 @@ const Events = () => {
               </div>
             </div>
 
-            <div  className=" absolute top-[20%] c1:top-[16%] glowing-div"></div>
+            {/* <div  className=" absolute top-[20%] c1:top-[16%] glowing-div"></div> */}
           </div>
-
-
-
-
 
           {/* day 2 */}
           <div className="">
             <div className="">
-                <img className="w-[38.32%] absolute top-[31.5%] uxl:top-[24.5%] c1:top-[23%] left-[9%]" src={img4}></img>
-                <p className="z-10 w-[10%] text-white font-cyntara absolute text-[1.2rem] xl:text-[1.5rem] uxl:text-[2rem] c1:text-[2.7rem] top-[51%] xl:top-[51%] uxl:top-[45%] c1:top-[43%] left-[23%] c1:left-[22%] tracking-wider">NINA SUERTE</p>
+              <img
+                className="w-[38.32%] absolute top-[31.5%] uxl:top-[24.5%] c1:top-[23%] left-[9%]"
+                src={img4}
+              ></img>
+              <p className="z-10 w-[10%] text-white font-cyntara absolute text-[1.2rem] xl:text-[1.5rem] uxl:text-[2rem] c1:text-[2.7rem] top-[51%] xl:top-[51%] uxl:top-[45%] c1:top-[43%] left-[23%] c1:left-[22%] tracking-wider">
+                NINA SUERTE
+              </p>
             </div>
 
-            <div >
-              <img className="w-[23.75%] absolute top-[40.8%] uxl:top-[33.82%] c1:top-[31.82%] left-[26.18%]" src={img5}></img>
-              <p className="z-10 w-[15%] text-white font-cyntara absolute text-[1.2rem] xl:text-[1.5rem] uxl:text-[2rem] c1:text-[2.7rem] top-[52%] xl:top-[53%] uxl:top-[47%] c1:top-[45%] left-[39%] tracking-wider">MASK BOX</p>
-              </div>
+            <div>
+              <img
+                className="w-[23.75%] absolute top-[40.8%] uxl:top-[33.82%] c1:top-[31.82%] left-[26.18%]"
+                src={img5}
+              ></img>
+              <p className="z-10 w-[15%] text-white font-cyntara absolute text-[1.2rem] xl:text-[1.5rem] uxl:text-[2rem] c1:text-[2.7rem] top-[52%] xl:top-[53%] uxl:top-[47%] c1:top-[45%] left-[39%] tracking-wider">
+                MASK BOX
+              </p>
+            </div>
             <div className="">
-              <img className="w-[18.80%] absolute top-[43.4%] uxl:top-[36.4%] c1:top-[34.4%] left-[9.5%] xl:left-[7.5%] uxl:left-[5.5%] c1:left-[4%]" src={img6}></img>
-              <p className="z-10 w-[10%] text-white font-cyntara absolute text-[1.2rem] xl:text-[1.5rem] uxl:text-[2rem] c1:text-[2.7rem] top-[52%] xl:top-[53%] uxl:top-[46%] c1:top-[44.5%] left-[10%] xl:left-[8%] c1:left-[5%] tracking-wider ">PRERAK ANAND</p>
-              </div>
+              <img
+                className="w-[18.80%] absolute top-[43.4%] uxl:top-[36.4%] c1:top-[34.4%] left-[9.5%] xl:left-[7.5%] uxl:left-[5.5%] c1:left-[4%]"
+                src={img6}
+              ></img>
+              <p className="z-10 w-[10%] text-white font-cyntara absolute text-[1.2rem] xl:text-[1.5rem] uxl:text-[2rem] c1:text-[2.7rem] top-[52%] xl:top-[53%] uxl:top-[46%] c1:top-[44.5%] left-[10%] xl:left-[8%] c1:left-[5%] tracking-wider ">
+                PRERAK ANAND
+              </p>
+            </div>
 
-            <div
-              className="w-[44.76%] flex flex-col absolute top-[29.31%] uxl:top-[26.31%] c1:top-[25.31%] left-[56.47%]  ">
-              <div className="absolute    font-cynatar uxsm:text-s tracking-wide c1:text-[20rem] 2xl:text-[15rem] xl:text-[13rem] lg:text-[12rem] md:text-[9rem] sm:text-[8rem] xsm:text-[5rem] uxsm:text-[4.5rem] font-[400] textfamily plinth">
+            <div className="w-[44.76%] flex flex-col absolute top-[29.31%] uxl:top-[26.31%] c1:top-[25.31%] left-[56.47%]  ">
+              {/* <div className="absolute    font-cynatar uxsm:text-s tracking-wide c1:text-[20rem] 2xl:text-[15rem] xl:text-[13rem] lg:text-[12rem] md:text-[9rem] sm:text-[8rem] xsm:text-[5rem] uxsm:text-[4.5rem] font-[400] textfamily plinth">
                 DAY 2
               </div>
               <div className="  font-cynatar mr-4 c1:text-[19rem] tracking-wide 2xl:text-[15rem] xl:text-[13rem] lg:text-[12rem] md:text-[9rem] sm:text-[8rem] xsm:text-[5rem] uxsm:text-[4.5rem] font-[400] textfamily3 z-1 plinth">
@@ -92,7 +140,35 @@ const Events = () => {
                 <p className="c1:text-[2.3rem] xl:text-[1.5rem] lg:text-[1.2rem] md:text-[1rem] sm:text-[1.2rem] xsm:text-[1rem] text-white font-melete font-[400]">
                   JAN 25,2025
                 </p>
-              </div>
+              </div> */}
+
+              <motion.div
+                className="absolute font-cynatar uxsm:text-s tracking-wide c1:text-[20rem] 2xl:text-[14rem] xl:text-[12rem] lg:text-[12rem] md:text-[9rem] sm:text-[8rem] xsm:text-[5rem] uxsm:text-[4.5rem] font-[400] textfamily plinth z-10"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                variants={animationVariants_bottom}
+              >
+                Day 2
+              </motion.div>
+              <motion.div
+                className="font-cynatar mr-4 c1:text-[20rem] tracking-wide 2xl:text-[14rem] xl:text-[12rem] lg:text-[12rem] md:text-[9rem] sm:text-[8rem] xsm:text-[5rem] uxsm:text-[4.5rem] font-[400] textfamily3 plinth z-50"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                variants={animationVariants}
+              >
+                Day 2
+              </motion.div>
+              <motion.p
+                className="c1:text-[2.3rem] xl:text-[1.5rem] lg:text-[1.2rem] md:text-[1rem] sm:text-[1.2rem] xsm:text-[1rem] text-white font-melete font-[400] -mt-10 ml-7"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                variants={animationVariants}
+              >
+                JAN 25, 2025
+              </motion.p>
             </div>
 
             <div className="flex flex-row w-[44.76%] absolute top-[45%] md:top-[44%] uxl:top-[40%] c1:top-[37.7%] left-[56.47%] gap-x-[4%]">
@@ -111,33 +187,42 @@ const Events = () => {
                 SUNBURN CAMPUS
               </div>
             </div>
-            <div  className=" absolute top-[57%] uxl:top-[50.7%] c1:top-[46.5%] glowing-div"></div>
+            {/* <div  className=" absolute top-[57%] uxl:top-[50.7%] c1:top-[46.5%] glowing-div"></div> */}
           </div>
-
-
-
-
-
 
           {/* day3 */}
           <div className="">
-          <div
-              className="w-[44.76%] flex flex-col absolute top-[62%] uxl:top-[53.31%] c1:top-[51.31%] left-[35.47%]  ">
-              <div className="absolute    font-cynatar uxsm:text-s tracking-wide c1:text-[20rem] 2xl:text-[15rem] xl:text-[13rem] lg:text-[12rem] md:text-[9rem] sm:text-[8rem] xsm:text-[5rem] uxsm:text-[4.5rem] font-[400] textfamily plinth">
-                DAY 3
-              </div>
-              <div className="  font-cynatar mr-4 c1:text-[19rem] tracking-wide 2xl:text-[15rem] xl:text-[13rem] lg:text-[12rem] md:text-[9rem] sm:text-[8rem] xsm:text-[5rem] uxsm:text-[4.5rem] font-[400] textfamily3 z-1 plinth">
-                DAY 3
-              </div>
-              <div className=" -mt-10 ml-7">
-                <p className="c1:text-[2.3rem] xl:text-[1.5rem] lg:text-[1.2rem] md:text-[1rem] sm:text-[1.2rem] xsm:text-[1rem] text-white font-melete font-[400]">
-                  JAN 26,2025
-                </p>
-              </div>
+            <div className="w-[44.76%] flex flex-col absolute top-[62%] uxl:top-[53.31%] c1:top-[51.31%] left-[35.47%]  ">
+            <motion.div
+                className="absolute font-cynatar uxsm:text-s tracking-wide c1:text-[20rem] 2xl:text-[14rem] xl:text-[12rem] lg:text-[12rem] md:text-[9rem] sm:text-[8rem] xsm:text-[5rem] uxsm:text-[4.5rem] font-[400] textfamily plinth z-10"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                variants={animationVariants_bottom}
+              >
+                Day 3
+              </motion.div>
+              <motion.div
+                className="font-cynatar mr-4 c1:text-[20rem] tracking-wide 2xl:text-[14rem] xl:text-[12rem] lg:text-[12rem] md:text-[9rem] sm:text-[8rem] xsm:text-[5rem] uxsm:text-[4.5rem] font-[400] textfamily3 plinth z-50"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                variants={animationVariants}
+              >
+                Day 3
+              </motion.div>
+              <motion.p
+                className="c1:text-[2.3rem] xl:text-[1.5rem] lg:text-[1.2rem] md:text-[1rem] sm:text-[1.2rem] xsm:text-[1rem] text-white font-melete font-[400] -mt-10 ml-7"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                variants={animationVariants}
+              >
+                JAN 26, 2025
+              </motion.p>
             </div>
 
-
-            <div className="absolute w-[68%] top-[83%]  uxl:top-[70.99%] c1:top-[68.9%] left-[24%] uxl:left-[29.09%] c1:left-[25.09%]">
+            <div className="absolute top-[83%] uxl:top-[68%] c1:top-[65.9%] left-[24%] uxl:left-[29.09%] c1:left-[25.09%]">
               <p className="c1:text-[3.5rem] 2xl:text-[2.5rem] xl:text-[2.0rem] lg:text-[2.2rem] md:text-[1.7rem] sm:text-[1.2rem] xsm:text-[1rem] text-white font-melete font-[400]">
                 REVEALING SOON
               </p>
@@ -153,10 +238,23 @@ const Events = () => {
                 BOLLYWOOD NIGHT
               </div>
             </div>
-            <div  className=" absolute top-[87.8%] uxl:top-[74%] c1:top-[72%] glowing-div"></div>
+            {/* <div  className=" absolute top-[87.8%] uxl:top-[74%] c1:top-[72%] glowing-div"></div> */}
           </div>
         </div>
       </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
       {/* mobile */}
       <div className="md:hidden block">
